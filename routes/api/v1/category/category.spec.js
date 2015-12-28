c9ba75d3c0085f9fe9ec13cae474421da68934b3
njir <njir@naver.com>
2015-12-28 17:00:14 +0900

    Add *.spec.js

--- routes/api/v1/category/category.spec.js
+++ routes/api/v1/category/category.spec.js
@@ -3,6 +3,20 @@ var app = require('../../../../app');
 var request = require('supertest');
 var should = require('should');
 
+describe('GET /api/v1/category', function() {
+  it('should respond with JSON array', function(done) {
+    request(app)
+      .get('/api/v1/category')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Array);
+        done();
+      });
+  });
+});
+
 describe('GET /api/v1/category/add', function() {
   it('should respond with text/html', function(done) {
     request(app)
--- routes/api/v1/user/user.spec.js
+++ routes/api/v1/user/user.spec.js
@@ -3,19 +3,19 @@ var app = require('../../../../app');
 var request = require('supertest');
 var should = require('should');
 
-//describe('GET /api/v1/users', function() {
-//  it('should respond with JSON array', function(done) {
-//    request(app)
-//      .get('/api/v1/users')
-//      .expect(200)
-//      .expect('Content-Type', /json/)
-//      .end(function(err, res) {
-//        if (err) return done(err);
-//        res.body.should.be.instanceof(Array);
-//        done();
-//      });
-//  });
-//});
+describe('GET /api/v1/user', function() {
+  it('should respond with JSON array', function(done) {
+    request(app)
+      .get('/api/v1/user')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Array);
+        done();
+      });
+  });
+});
 
 describe('GET /api/v1/user/join', function() {
   it('should respond with text/html', function(done) {
